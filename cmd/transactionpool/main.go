7abb8b06d6d88f34fd6e0b9f54070c04c0ec14cd
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-03 00:45:19 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/transactionpool/main.go
@@ -0,0 +1,23 @@
+// cmd/transactionpool/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"transactionpool/internal/transactionpool"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := transactionpool.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
